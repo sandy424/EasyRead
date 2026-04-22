5bc88f090a286097cf1fcf18a45059c44f4f0690
DESKTOP-Q5CFOMV\user <sandy424@naver.com>
2026-04-22 19:39:17 +0900

    add: 회원가입 페이지 생성

---
+++ app/signup/page.tsx
@@ -0,0 +1,31 @@
+export default function signupPage() {
+    return(
+        <div className="bg-mint-10 w-full flex justify-center items-center scroll-hidden">
+            <div className="border border-transparent bg-mint-50 rounded-2xl w-[480px] h-[570px] m-20 flex flex-col items-center p-8">
+                <span className="text-teal-900 text-2xl font-semibold">
+                    EZREAD
+                </span>
+                <p className="text-teal-600 text-sm pt-3 font-medium">EZREAD에 오신 것을 환영합니다.</p>
+                <div className="flex flex-col gap-10 pt-16">
+                    <input type="text" placeholder="아이디 "className="border border-transparent rounded-xl bg-white/70 w-96 h-10 pl-7 font-[700] text-sm" />
+                    <input type="text" placeholder="8자 이상의 비밀번호를 입력하세요 "className="border border-transparent rounded-xl bg-white/70 w-96 h-10 pl-7 font-[700] text-sm" />
+                    <select name="" id="" className="border border-transparent rounded-xl bg-white/70 w-96 h-10 pl-7 font-[700] text-sm text-gray-400">
+                        <option value="">나만의 질문을 선택하세요</option>
+                        <option value="FAVORITE_FRUIT">가장 좋아하는 과일의 이름은?</option>
+                        <option value="BEST_FRIEND">가장 절친한 친구의 이름은?</option>
+                        <option value="FIRST_PET">처음 키운 반려동물의 이름은?</option>
+                        <option value="FAVORITE_TEACHER">가장 존경하는 선생님의 이름은?</option>
+                        <option value="BIRTH_CITY">태어난 도시는?</option>
+                    </select>
+                    <input type="text" placeholder="나만의 질문에 대한 답을 적으세요 "className="border border-transparent rounded-xl bg-white/70 w-96 h-10 pl-7 font-[700] text-sm" />
+                </div>
+                <button className="px-6 py-2 border border-transparent bg-mint-100 rounded-lg text-mint-900 font-semibold text-base cursor-pointer hover:bg-mint-600 mt-8">
+                    로그인
+                </button>
+                <div className="flex text-xs pt-4">
+                    <a href="/login" className="text-teal-700 font-semibold">로그인하러 가기</a>
+                </div>
+            </div>
+        </div>
+    )
+}
